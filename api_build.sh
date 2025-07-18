3e4b9621900f86c0b9d45d3b02daf80249438e59
FredericOlfa <frederic.thome@olfa.fr>
2025-07-18 11:53:52 +0200

    fix : erreur api_build.sh

--- api_build.sh
+++ api_build.sh
@@ -7,7 +7,7 @@ sudo docker run --env=GPG_KEY=A035C8C19219BA821ECEA86B64E628F8D684696D \
                 --env=PYTHON_VERSION=3.10.18 \
                 --env=PYTHON_SHA256=ae665bc678abd9ab6a6e1573d2481625a53719bc517e9a634ed2b9fefae3817f \
                 --env=OKANBAN_CONSOLE_LEVEL="INFO" \
-                --env=OKANBAN_LOGFILE_LEVEL="INFO"
+                --env=OKANBAN_LOGFILE_LEVEL="INFO" \
                 --env=OKANBAN_MONGODB_HOST = '192.168.0.20' \
                 --env=OKANBAN_MONGODB_PORT = 27017 \
                 --env=OKANBAN_NICELABEL_URL = 'http://192.168.0.16:56426' \
